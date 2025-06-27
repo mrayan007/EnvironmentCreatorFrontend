d00f21b1cfae9c3e539a0d2d99cfd5c8d3e33882
mrayan007 <rayanwmv@gmail.com>
2025-06-27 20:00:27 +0200

    projeto finito

--- Frontend/Assets/Scripts/ObjectManager.cs
+++ Frontend/Assets/Scripts/ObjectManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,59 +8,86 @@ public class ObjectManager : MonoBehaviour
 {
     // Menu om objecten vanuit te plaatsen
     public GameObject UISideMenu;
-    // Lijst met objecten die geplaatst kunnen worden die overeenkomen met de prefabs in de prefabs map
+    // Lijst met objecten die geplaatst kunnen worden (matchen met prefab namen)
     public List<GameObject> prefabObjects;
 
-    // Lijst met objecten die geplaatst zijn in de wereld
+    // Alle geplaatste objecten (oud + nieuw)
     private List<GameObject> placedObjects = new List<GameObject>();
+    // Alleen nieuw toegevoegde objecten
+    private List<GameObject> newObjects = new List<GameObject>();
+
+    private void Start()
+    {
+        var currentEnvironment = CurrentEnvironment.currentEnvironment;
+        var objects = currentEnvironment.objects;
+
+        foreach (var objDto in objects)
+        {
+            GameObject prefab = prefabObjects.FirstOrDefault(p => p.name == objDto.prefabId);
+            if (prefab == null) continue;
+
+            Vector3 position = new Vector3(objDto.positionX, objDto.positionY, 0f);
+            Quaternion rotation = Quaternion.Euler(0f, 0f, objDto.rotationZ);
+            GameObject instance = Instantiate(prefab, position, rotation);
+            instance.transform.localScale = new Vector3(objDto.scaleX, objDto.scaleY, 1f);
+
+            var renderer = instance.GetComponent<SpriteRenderer>();
+            if (renderer != null)
+                renderer.sortingOrder = objDto.sortingLayer;
+
+            Object2D object2D = instance.GetComponent<Object2D>();
+            object2D.objectManager = this;
+            object2D.prefabId = prefab.name;
+
+            placedObjects.Add(instance); // Alleen in de algemene lijst, want dit zijn oude objecten
+        }
+    }
 
     // Methode om een nieuw 2D object te plaatsen
     public void PlaceNewObject2D(int index)
     {
-        // Verberg het zijmenu
         UISideMenu.SetActive(false);
-        // Instantieer het prefab object op de positie (0,0,0) met geen rotatie
+
         GameObject prefab = prefabObjects[index];
         GameObject instanceOfPrefab = Instantiate(prefab, Vector3.zero, Quaternion.identity);
-        // Haal het Object2D component op van het nieuw geplaatste object
-        Object2D object2D = instanceOfPrefab.GetComponent<Object2D>();
-        // Stel de objectManager van het object in op deze instantie van ObjectManager
-        object2D.objectManager = this;
-        // Zet de isDragging eigenschap van het object op true zodat het gesleept kan worden
-        object2D.isDragging = true;
 
+        Object2D object2D = instanceOfPrefab.GetComponent<Object2D>();
+        object2D.objectManager = this;
+        object2D.isDragging = true;
         object2D.prefabId = prefab.name;
 
         placedObjects.Add(instanceOfPrefab);
+        newObjects.Add(instanceOfPrefab); // Alleen nieuwe objecten opslaan later
     }
 
-    // Methode om het menu te tonen
     public void ShowMenu()
     {
         UISideMenu.SetActive(true);
     }
 
-    // Methode om de huidige scène te resetten
     public void Reset()
     {
-        // Laad de huidige scène opnieuw
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        foreach (var obj in placedObjects)
+        {
+            if (obj != null)
+                Destroy(obj);
+        }
+
+        placedObjects.Clear();
+        newObjects.Clear();
     }
 
     public async void Save()
     {
         var environmentId = CurrentEnvironment.currentEnvironment.id;
 
-        foreach (var obj in placedObjects)
+        foreach (var obj in newObjects) // Alleen nieuwe objecten opslaan
         {
             ObjectDto dto = new ObjectDto();
-
             Object2D object2D = obj.GetComponent<Object2D>();
 
-            // Use the tracked prefab name
             dto.prefabId = object2D.prefabId;
 
-            // Transform data
             Vector3 pos = obj.transform.position;
             Vector3 scale = obj.transform.localScale;
             float rotZ = obj.transform.eulerAngles.z;
@@ -70,17 +98,14 @@ public class ObjectManager : MonoBehaviour
             dto.scaleY = scale.y;
             dto.rotationZ = rotZ;
 
-            // Sorting layer
             var renderer = obj.GetComponent<SpriteRenderer>();
             dto.sortingLayer = renderer != null ? renderer.sortingOrder : 0;
 
-            // Environment ID
             dto.environmentId = environmentId;
 
-            // Send to API
             await ApiClient.instance.AddObject(dto);
         }
 
-        Debug.Log("All objects saved.");
+        Debug.Log("New objects saved.");
     }
 }
